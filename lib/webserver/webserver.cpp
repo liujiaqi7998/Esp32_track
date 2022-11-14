92789779f81131377bfaf3a6ac5552dbfaa431ee
liujiaqi7998 <1792498113@qq.com>
2022-11-14 15:09:01 +0800

    websocket

--- lib/webserver/webserver.cpp
+++ lib/webserver/webserver.cpp
@@ -5,12 +5,52 @@
 #include <WiFi.h>
 
 AsyncWebServer server(80);
+AsyncWebSocket ws("/ws"); // WebSocket对象，url为/
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+    // Handle WebSocket event
+    // if (type == WS_EVT_CONNECT)
+    // {
+    //     // client connected
+    //     client->printf("Hello Client %u :)", client->id());
+    //     client->ping();
+    // }
+    
+}
+
+
+void trank_Task(void *parameter)
+{
+    Serial.println("[线程管理]:启动线程");
+    
+    while (1)
+    {
+        ws.printfAll("123");
+    }
+    
+}
+
 
 void int_web()
 {
     
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(200, "text/html", main_HTML); });
+              
     DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
     AsyncElegantOTA.begin(&server); // Start AsyncElegantOTA
-                                    // attach AsyncWebSocket
+                                    
+    ws.onEvent(onEvent);
+    
+    server.addHandler(&ws);// attach AsyncWebSocket
     server.begin();
+
+    xTaskCreate(
+            trank_Task,   /* Task function. */
+            "trank_Task", /* String with name of task. */
+            10000,                 /* Stack size in bytes. */
+            NULL,                  /* Parameter passed as input of the task */
+            1,                     /* Priority of the task. */
+            NULL);                 /* Task handle. */
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ void setup()
   Serial.println("EN's Varocol and 666Qi track");
   pinMode(2, OUTPUT); // 初始化LED引脚为输出模式
   
-  //当按下 IO0 按钮，进入菜单
+  //当按下 IO0 按钮，进入配网模式
   pinMode(0, INPUT);
   attachInterrupt(0, network_config_begin, FALLING); // 设置外部中断
 
